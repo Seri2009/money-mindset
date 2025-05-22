07f9217db514a5876a0a84af85df3c2ba8727e10
KadTheAad <69099988+KadTheAad@users.noreply.github.com>
2025-05-22 18:48:36 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/money-mindest-static-bloom/",
+  base: "/clean-blog-website-template/",
   server: {
     host: "::",
     port: 8080,
