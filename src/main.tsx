4d0b7600ea01069a3448c0a65ef260ed5b6d415f
KadTheAad <69099988+KadTheAad@users.noreply.github.com>
2025-06-08 20:31:20 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import App from './App.tsx'
 import './index.css'
 import posthog from 'posthog-js'
 
-posthog.init(
-  'phc_Y1nGZrdU8Uj0fJ6wR2G1upukDucN1Xlecdi9jTqgkfZ',
-  {
-    api_host: 'https://us.i.posthog.com'
+const posthogKey = 'phc_Y1nGZrdU8Uj0fJ6wR2G1upukDucN1Xlecdi9jTqgkfZ';
+let apiHost = process.env.HOST1;
+
+async function initPostHog() {
+  if (posthogKey && apiHost) {
+    try {
+      await fetch(apiHost, { method: "HEAD", mode: 'no-cors'});
+    } catch (error) {
+      apiHost = "https://{yourdomain}.com/{your-worker-prefix}";
+    }
+    posthog.init(posthogKey, {
+      api_host: apiHost,
+      person_profiles: "identified_only",
+    });
   }
-)
+}
+
+initPostHog();
 
 createRoot(document.getElementById("root")!).render(<App />);
